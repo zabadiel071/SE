e493e763ee83bdafed477e20da926193a47a7555
naujoh <nauj_sod2@hotmail.com>
2017-11-30 22:36:05 -0600

    Adding inference engine class

--- SE_IA/src/main/java/inference/Justification.java
+++ SE_IA/src/main/java/inference/Justification.java
@@ -1,4 +1,9 @@
 package inference;
 
+import java.util.ArrayList;
+
 public class Justification {
+    ArrayList<String> fact_base;
+
+    public void print(){}
 }
--- SE_IA/src/main/java/knowledgeBase/Rule.java
+++ SE_IA/src/main/java/knowledgeBase/Rule.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Rule {
     /**
      *
-      */
+     */
    private ArrayList<String> background;
 
     /**
