54c7d4f6fa3aea1055e13d6d7c2e35e474714630
Marco Vazquez <marcovazquez77@hotmail.com>
2018-09-10 01:56:04 -0500

    Not found and result not found output added to constants

--- SE_IA/src/main/java/files/Constants.java
+++ SE_IA/src/main/java/files/Constants.java
@@ -16,6 +16,8 @@ public class Constants {
     public static byte REGISTER_BACKGROUND = 10;
     //Master data
     public static byte POSITION_LENGTH = Long.BYTES;
+    public static String NOT_FOUND = "404";
+    public static String INIT_STATE_NOT_FOUND = "No se ha encontrado el genero :(";
 
     private static String NO_DATA(){
         char[] chars = new char[GENRE_KEY_LENGTH];
